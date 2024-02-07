402128227d0b6a23ad2b25ca00f1a2c8765e25ff
do990118 <doe990118@naver.com>
2024-02-07 15:15:17 +0900

    getNode불러오기전

--- client/main.js
+++ client/main.js
@@ -1,4 +1,13 @@
-console.log('안녕 자바스크립트!')
+// 1. input value 값 가져오기
 
-const a = 10
+const first = getNode('#firstNumber');
+const second = getNode('#secondNumber');
 
+// 2. 두 수의 합 더하기
+
+function handleInput() {
+  console.log('this.value');
+}
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
